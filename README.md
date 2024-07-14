16c0d711e724f6225eacc6a413db1a95b47c6bfd
Stephen <stephen.loucel@gmail.com>
2024-07-14 16:22:27 -0400

    added satellite map

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Module 15 Challenge
 
 # Timeline
 Sun, July 14th @ 2:30PM EST - Created Repo and add starter files
-Sun, July 14th @ 
+Sun, July 14th @ 4:10PM EST - Lost progress and had to start again
+Sun, July 14th @ 4:20PM EST - Added Satellite to the map
 
 # Resources
 * 15-Mapping/3/Activities/10-Stue_GeoJson
+* Chat GPT to find a map that did not require a token
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL/
+// Perform a GET request to the query URL.
 d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
@@ -21,24 +21,24 @@ function createFeatures(earthquakeData) {
     onEachFeature: onEachFeature
   });
 
-  // Send our earthquakes layer to the createMap function/
+  // Send our earthquakes layer to the createMap function.
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
+  // Create the Esri satellite base layer.
+  let satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles © Esri'
+  });
+  
   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
   // Create a baseMaps object.
   let baseMaps = {
-    "Street Map": street,
+    "Satellite Map": satellite,
     "Topographic Map": topo
   };
 
@@ -47,13 +47,13 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  // Create our map, giving it the satellite and earthquakes layers to display on load.
   let myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [street, earthquakes]
+    layers: [satellite, earthquakes]
   });
 
   // Create a layer control.
