3cbfbfaa03a2c0290ab68bd99ef84c68414d65d6
Stephen <stephen.loucel@gmail.com>
2024-07-14 19:12:33 -0400

    added circleMarker and color scale.

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Module 15 Challenge
 Sun, July 14th @ 2:30PM EST - Created Repo and add starter files
 Sun, July 14th @ 4:10PM EST - Lost progress and had to start again
 Sun, July 14th @ 4:20PM EST - Added Satellite to the map
+Sun, July 14th @ 4:31PM EST - Added Grayscale and Outdoors maps. 
+Sun, July 14th @ 7:11PM EST - Added circleMarker and color scale based on magnitude.
 
 # Resources
-* 15-Mapping/3/Activities/10-Stue_GeoJson
+* Starting Point for Earthquack map: 15-Mapping/3/Activities/10-Stue_GeoJson
 * Chat GPT to find a map that did not require a token
--- static/js/logic.js
+++ static/js/logic.js
@@ -9,15 +9,45 @@ d3.json(queryUrl).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
+  // Define a function to create a color scale based on magnitude.
+  function createColorScale(minColor, maxColor, numSplits) {
+    const colorScale = d3.scaleLinear()
+      .domain([0, numSplits])
+      .range([minColor, maxColor])
+      .interpolate(d3.interpolateHcl);
+
+    return function(magnitude) {
+      const splitIndex = Math.min(Math.floor(magnitude), numSplits);
+      return colorScale(splitIndex);
+    };
+  }
+
+  // Use the color scale function to generate colors.
+  const getColor = createColorScale('#90ee90', '#c04040', 5); // green to red with 5 splits
+
+  // Define a function to determine the radius of the marker based on the earthquake magnitude.
+  function getRadius(magnitude) {
+    return magnitude * 4;
+  }
+
+  // Define a function to create circle markers for each feature in the features array.
   function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><p>Magnitude: ${feature.properties.mag}</p>`);
   }
 
   // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+  // Create circle markers instead of default markers.
   let earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+      return L.circleMarker(latlng, {
+        radius: getRadius(feature.properties.mag),
+        fillColor: getColor(feature.properties.mag),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      });
+    },
     onEachFeature: onEachFeature
   });
 
@@ -31,11 +61,13 @@ function createMap(earthquakes) {
   let satellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles © Esri'
   });
-  
+
+  // Create the grayscale base layer.
   let grayscale = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
   });
 
+  // Create the outdoor base layer.
   let outdoors = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
   });
@@ -68,4 +100,23 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 
+  // Add a legend to the map.
+  let legend = L.control({ position: 'bottomright' });
+
+  legend.onAdd = function (map) {
+    let div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5, 6],
+        labels = [];
+
+    // Loop through our density intervals and generate a label with a colored square for each interval.
+    for (let i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
 }
